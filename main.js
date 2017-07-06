701f40c964681d9691fdfd98f4ef669b7c9ad08b
Grant McGrath <grantmcgrath@me.com>
2017-07-06 11:20:21 -0400

    Completed assignment and compared to solution file

--- main.js
+++ main.js
@@ -2,8 +2,8 @@
 // Complete the following function
 // It should return `true` if the first argument is greater than the second and `false` if not
 // Use the greater than operator `>`
-function greaterThan (num1, num2) {
-  return;
+function greaterThan(num1, num2) {
+  return num1 > num2;
 }
 
 console.log(greaterThan(3, 2) === true);
@@ -12,30 +12,42 @@ console.log(greaterThan(3, 2) === true);
 // 2.
 // Call the `greaterThan` function in the condition for the following if statement
 // Provide arguments to `greaterThan` which make the if statement log 'Less than or equal to'
-if (greaterThan()) {
+if (greaterThan(3, 3)) {
   console.log('Greater than');
 } else {
   console.log('Less than or equal to');
 }
-
 
 // 3.
 // Refactor the `lessThan` function to match the following specs:
 // It returns 'Less than' if the first argument is less than the second
 // It returns 'Greater than' if the first argument is greater than the second
 // It returns 'Equal to' if the first argument is equal to the second
-function lessThan (num1, num2) {
+function lessThan(num1, num2) {
   // Your answer here
+
+  if (num1 < num2) {
+    return "Less than";
+  } else if (num1 === num2) {
+    return "Equal to";
+  } else if (num1 > num2) {
+    return "Greater than";
+  }
 }
 
-console.log(lessThan(2, 3) === 'Less than');
-console.log(lessThan(2, 1) === 'Greater than');
-console.log(lessThan(2, 2) === 'Equal to');
+  console.log(lessThan(2, 3) === 'Less than');
+  console.log(lessThan(2, 1) === 'Greater than');
+  console.log(lessThan(2, 2) === 'Equal to');
 
 
-// 4.
-// The below assertion does not work. 
-// The function addNumbers needs to be created
-let twoPlusTwo = addNumbers(2, 2);
+  // 4.
+  // The below assertion does not work.
+  // The function addNumbers needs to be created
+  function addNumbers(num1, num2) {
+    return num1 + num2;
+  }
 
-console.log(twoPlusTwo === 4)
+  let twoPlusTwo = addNumbers(2, 2);
+
+
+  console.log(twoPlusTwo === 4)
